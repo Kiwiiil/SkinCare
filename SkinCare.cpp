292a229f7406c287c3c441a5f695decbc6daa6cc
Kiwiiil <rizkydwis980@gmail.com>
2024-10-04 17:29:10 +0700

    Update SkinCare.cpp

--- SkinCare.cpp
+++ SkinCare.cpp
@@ -1,24 +1,30 @@
 #include <iostream>
 using namespace std;
 
-void skinc (string facialw, int moizturizer){
-}
-int main (){
-    int skinca;
-    string facialw;
-    int moizturizer;
-    cout << "Pilih menu skin care yang kamu inginkan :" << endl;
-    cout << "1. Facial Wash" << endl << "2. Moizturizer" << endl;
-    cin >> skinca;
-    if (skinca == 1){
-        cout << "Pilih Facial Wash : " << endl;
-        cout << "1.    " << endl << "2.    " << endl;
-    } else if (skinca == 2){
-        cout << "Pilih Moiturizer : " << endl;
-        cout << "1.    " << endl << "2.    " << endl;
-    }
-    skinc (facialw, moizturizer);
+int a,b,c;
+void skinc() {
 
-    cin.get();
+    cout << "Gender :" << endl << "1. Pria" << endl << "2. Wanita" << endl;
+    cout << "Pilih Angka : ";
+    cin >> a;
+    cout << endl;
+
+    switch (a) {
+        case 1:
+        cout << "Skin Type : " << endl;
+        cout << "1. Normal" << endl;
+        cout << "2. Dry" << endl;
+        cout << "3. Oily" << endl;
+        cout << "4. Combination" << endl;
+        cout << "5. Acne Prone Skin (dry)" << endl << "6. Acne Prone Skin (Oily)" << endl;
+        cout << "7. kembali" << endl;
+        cout << "Pilih Angka : ";
+        cin >> a;
+    }
+}
+
+int main () {
+
+    skinc();
     return 0;
 }
