009eda72328d62b5641d1aae63d9603485215a04
Kiwiiil <rizkydwis980@gmail.com>
2024-10-04 18:17:44 +0700

    Update SkinCare.cpp

--- SkinCare.cpp
+++ SkinCare.cpp
@@ -6,20 +6,32 @@ void skinc() {
 
     cout << "Gender :" << endl << "1. Pria" << endl << "2. Wanita" << endl;
     cout << "Pilih Angka : ";
-    cin >> a;
+    cin >> a; 
     cout << endl;
-
-    switch (a) {
-        case 1:
+    if (a == 1) {
         cout << "Skin Type : " << endl;
         cout << "1. Normal" << endl;
         cout << "2. Dry" << endl;
         cout << "3. Oily" << endl;
         cout << "4. Combination" << endl;
         cout << "5. Acne Prone Skin (dry)" << endl << "6. Acne Prone Skin (Oily)" << endl;
-        cout << "7. kembali" << endl;
-        cout << "Pilih Angka : ";
-        cin >> a;
+        cout << "Pilih Angka : "; 
+    } else if (a == 2) {
+        cout << "Skin Type : " << endl;
+        cout << "1. Normal" << endl;
+        cout << "2. Dry" << endl;
+        cout << "3. Oily" << endl;
+        cout << "4. Combination" << endl;
+        cout << "5. Acne Prone Skin (dry)" << endl << "6. Acne Prone Skin (Oily)" << endl;
+        cout << "Pilih Angka : "; 
+    } else {
+        cout << "Angka yang anda masukkan tidak ada di dalam menu pilihan." << endl;
+    }
+    cin >> a;
+    if ( a < 7) {
+        cout << "Hai" << endl;
+    } else {
+        cout << "Angka yang anda masukkan tidak ada di dalam menu pilihan." << endl;
     }
 }
 
